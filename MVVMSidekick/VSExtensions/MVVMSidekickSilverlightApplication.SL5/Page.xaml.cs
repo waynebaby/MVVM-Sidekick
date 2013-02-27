a1a4c449d8f6eb9a71243a85bc852b500567de17
Wayne Wang 韦恩卑鄙 <blackshaman_wayne@hotmail.com>
2013-02-27 14:26:20 +0800

    Silverlight templates finished. Only WP8 to go

---
+++ MVVMSidekick/VSExtensions/MVVMSidekickSilverlightApplication.SL5/Page.xaml.cs
@@ -0,0 +1,30 @@
+﻿using MVVMSidekick.Views;
+using MVVMSidekick.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using $safeprojectname$.ViewModels;
+
+namespace $safeprojectname$
+{
+    public partial class MainPage : MVVMControl
+    {
+        public MainPage():base(null)
+        {
+            InitializeComponent();
+        }
+
+        public MainPage(MainPage_Model model):base(model)
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MVVMSidekick/VSExtensions/MVVMSidekickViewWithVM.SL5/View.xaml.cs
@@ -0,0 +1,41 @@
+﻿using MVVMSidekick.Views;
+using MVVMSidekick.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+using $rootnamespace$.ViewModels;
+namespace $rootnamespace$
+{
+    public partial class $safeitemname$ : MVVMPage
+    {
+
+
+        
+        public $safeitemrootname$():base(null)
+        {
+            this.InitializeComponent();
+        }
+        public $safeitemrootname$($safeitemrootname$_Model model):base(model)
+        {
+            this.InitializeComponent();
+        }
+               // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+    }
+}
+
+
+
+
