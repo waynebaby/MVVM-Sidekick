373a0d7273fb59db90e98f464dda4bdfb2f3660f
waynebaby <blackshaman_wayne@hotmail.com>
2013-12-14 00:02:21 +0800

    update snippets

--- MVVMSidekick/Nuget/MVVM-Sidekick.Behaviors/src/CodeFiles/Properties/AssemblyInfo.cs
+++ MVVMSidekick/Nuget/MVVM-Sidekick.Behaviors/src/CodeFiles/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MVVMSidekickViewWithVM_81.ModenUI")]
+[assembly: AssemblyTitle("MVVMSidekick.Behaviors_net40")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MVVMSidekickViewWithVM_81.ModenUI")]
+[assembly: AssemblyProduct("MVVMSidekick.Behaviors_net40")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1f9dd3b6-4027-41f2-9363-baea3f9bafe7")]
+[assembly: Guid("f6284605-08cb-40de-8783-ec4217b1670f")]
 
 // Version information for an assembly consists of the following four values:
 //
