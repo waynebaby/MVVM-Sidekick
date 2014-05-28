163953ea1431c206449a750dab221fab2ba37203
waynebaby <blackshaman_wayne@hotmail.com>
2014-05-29 00:03:15 +0800

    Universal almost done. MainPage need more work

---
+++ MVVMSidekick/VSExtensions/MVVMSidekickUniversalApplication/Shared/Startups/StartupFunctions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection ;
+namespace $ext_safeprojectname$.Startups
+{
+    public static partial class StartupFunctions
+    {
+        
+        public static void RunAllConfig()
+        {
+
+            typeof(StartupFunctions)
+                    .GetRuntimeMethods()
+                    .Where(m => m.Name.StartsWith("Config") && m.IsStatic)
+                    .Select(                      
+                        m => m.Invoke(null, Enumerable.Empty<object>().ToArray()))
+                    .ToArray();
+            
+        }
+
+    }
+}
