e9db56b2d43e99af622daeac92d07b9550592b4f
waynebaby <blackshaman_wayne@hotmail.com>
2014-06-06 14:06:07 +0800

    fix

--- MVVMSidekick/VSExtensions/MVVMSidekickWindowsPhoneApplication.WP8/Properties/AssemblyInfo.cs
+++ MVVMSidekick/VSExtensions/MVVMSidekickWindowsPhoneApplication.WP8/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MVVMSidekickWindowsPhoneApplication.WP8")]
+[assembly: AssemblyTitle("$projectname$")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MVVMSidekickWindowsPhoneApplication.WP8")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("$registeredorganization$")]
+[assembly: AssemblyProduct("$projectname$")]
+[assembly: AssemblyCopyright("Copyright © $registeredorganization$ $year$")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3d33ceab-ec5e-4602-b45f-daed6411e154")]
+[assembly: Guid("$guid3$")]
 
 // Version information for an assembly consists of the following four values:
 //
