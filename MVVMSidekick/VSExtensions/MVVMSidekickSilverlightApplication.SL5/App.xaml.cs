b9f43387f50c2d3d873587de676dcabe7d90975a
waynebaby <DACYDACY123a!>
2014-07-01 12:20:57 +0800

    fix sl namespace in Mainpage.xaml.cs

--- MVVMSidekick/VSExtensions/MVVMSidekickSilverlightApplication.SL5/App.xaml.cs
+++ MVVMSidekick/VSExtensions/MVVMSidekickSilverlightApplication.SL5/App.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
-using $safeprojectname$.Startups;
+
 namespace $safeprojectname$
 {
     public partial class App: Application 
