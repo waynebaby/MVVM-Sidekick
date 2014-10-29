f44eed0c9fdc9ef16fac68d1927489039460a558
waynebaby <blackshaman_wayne@Hotmail.com>
2014-10-30 00:08:25 +0800

    fix version number

--- MVVMSidekick/Nuget/MVVM-Sidekick/laucher.cmd
+++ MVVMSidekick/Nuget/MVVM-Sidekick/laucher.cmd
@@ -4,7 +4,7 @@ set "sver=%sver::=%"
 set "sver=%sver:.=%"
 
 
-set bver=0%date%
+set bver=%date%
 set "bver=%bver::=%"
 set "bver=%bver:/=%"
 set "bver=%bver:.=%"
@@ -12,8 +12,8 @@ set "bver=%bver:.=%"
 set "bver=%bver:~0,8%"
 set "bver=%bver: =%"
 
-rem set ver=1.3.%bver%.%sver%
-set ver=1.3.20141026.4090168
+ set ver=1.3.%bver%.%sver%
+rem set ver=1.3.20141026.4090168
 echo %ver%
 xcopy lib\*.*  ..\..\packages\MVVM-Sidekick.%ver%\lib  /s /i /y 
 BuildPublishPackage.cmd MVVM-Sidekick "%ver%"
