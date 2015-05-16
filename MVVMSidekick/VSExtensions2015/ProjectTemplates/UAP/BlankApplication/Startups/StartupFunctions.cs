607f99eb2d2bb9aba9418fede2d192830d64649e
waywa msft <blackshaman_wayne@hotmail.com>
2015-05-16 23:32:23 +0800

    Create  vs2015  extension and finished first ver of  Windows Universal Template

---
+++ MVVMSidekick/VSExtensions2015/ProjectTemplates/UAP/BlankApplication/Startups/StartupFunctions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection ;
+namespace MVVMSidekick.Startups
+{
+    internal static partial class StartupFunctions
+    {
+        
+       
+      	static List<Action> AllConfig ;
+
+		public static Action CreateAndAddToAllConfig(this Action action)
+		{
+			if (AllConfig == null)
+			{
+				AllConfig = new List<Action>();
+			}
+			AllConfig.Add(action);
+			return action;
+		}
+		public static void RunAllConfig()
+		{
+			if (AllConfig==null) return;
+			foreach (var item in AllConfig)
+			{
+				item();
+			}
+
+		}
+
+
+    }
+}
