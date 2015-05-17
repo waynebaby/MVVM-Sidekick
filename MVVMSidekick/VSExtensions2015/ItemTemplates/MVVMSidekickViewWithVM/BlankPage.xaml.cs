54790428266f50365142742f1603e42b77194b84
waywa msft <blackshaman_wayne@hotmail.com>
2015-05-17 09:43:24 +0800

    Add vs2015  item templates

---
+++ MVVMSidekick/VSExtensions2015/ItemTemplates/MVVMSidekickViewWithVM/BlankPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿
+using $rootnamespace$.ViewModels;
+using System.Reactive;
+using System.Reactive.Linq;
+using MVVMSidekick.ViewModels;
+using MVVMSidekick.Views;
+using MVVMSidekick.Reactive;
+using MVVMSidekick.Services;
+using MVVMSidekick.Commands;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace $rootnamespace$
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class $safeitemname$ : MVVMPage
+    {
+	
+		public $safeitemname$()
+            : this(null)
+        {
+            this.InitializeComponent();
+        }
+        public $safeitemname$($safeitemname$_Model model)
+            : base(model)
+        {
+            this.InitializeComponent();
+        }
+
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+        }
+	   
+    }
+}
