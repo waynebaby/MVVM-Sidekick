7bac2737af2fb543ce618bcca61dd6cd9fa1b0b0
waywa msft <blackshaman_wayne@hotmail.com>
2015-05-24 02:47:09 +0800

    update WPF templates

---
+++ MVVMSidekick/VSExtensions2015/ProjectTemplates/Windows Desktop/MVVMSidekickApplication.WPF/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Reactive;
+using System.Reactive.Linq;
+using MVVMSidekick.ViewModels;
+using MVVMSidekick.Views;
+using MVVMSidekick.Reactive;
+using MVVMSidekick.Services;
+using MVVMSidekick.Commands;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$$if$ ($targetframeworkversion$ >= 4.5)using System.Threading.Tasks;
+$endif$using System.Windows;
+
+namespace $safeprojectname$
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static void InitNavigationConfigurationInThisAssembly()
+		{
+			MVVMSidekick.Startups.StartupFunctions.RunAllConfig();
+		}
+
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			InitNavigationConfigurationInThisAssembly();
+		}
+    }
+}
