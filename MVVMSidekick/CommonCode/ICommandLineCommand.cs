7d7d5023f05aed454a725d6510d8b24ea6cc2a03
waynebaby <blackshaman_wayne@hotmail.com>
2015-07-24 23:44:39 +0800

    start build tool conmmand line project

---
+++ MVVMSidekick/CommonCode/ICommandLineCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonCode
+{
+	public interface ICommandLineCommand
+	{
+		string CommandKeyword { get; }
+
+		void Execute(string[] args);
+
+		void Help();
+
+		ICommandLineCommand Parent { get; }
+
+		IDictionary<string, ICommandLineCommand> Children { get; }
+	}
+
+	public class CommandLineCommand : ICommandLineCommand
+	{
+
+
+		public CommandLineCommand(string commandKeyword, ICommandLineCommand parent)
+		{
+			CommandKeyword = commandKeyword;
+			Parent = parent;
+		}
+
+		public virtual IDictionary<string, ICommandLineCommand> Children { get; private set; }
+			= new SortedDictionary<string, ICommandLineCommand>(
+				Comparer<string>
+					.Create((x, y) => string.Compare(x, y, StringComparison.InvariantCultureIgnoreCase)));
+
+
+		public virtual string CommandKeyword { get; }
+		public virtual ICommandLineCommand Parent { get; }
+
+
+		public Action<string[]> OnExecute { get; set; } = args => { };
+		public void Execute(string[] args)
+		{
+			if (OnExecute != null)
+			{
+				OnExecute(args);
+			}
+		}
+
+		public Action OnHelp { get; set; } = () => { };
+		public void Help()
+		{
+			if (OnHelp != null)
+			{
+				OnHelp();
+			}
+		}
+	}
+}
---
+++ MVVMSidekick/CommonCode/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonCode
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+
+
+																					
+			
+		}	 
+	}
+
+
+	
+}
