488bfbea5ee17fd226746b09b6c85931936c6bdd
waynebaby <blackshaman_wayne@hotmail.com>
2016-12-19 21:45:06 +0800

    Revert "updating nuget packages  to rx 3.00"
    This reverts commit a1890b87cd6526d0800ff593ced0aa8f959ae827.

--- MVVMSidekick/MVVMSidekick.Shared/Behaviors/BindToAppliactionBarIconButtonBehavior.cs
+++ MVVMSidekick/MVVMSidekick.Shared/Behaviors/BindToAppliactionBarIconButtonBehavior.cs
@@ -265,7 +265,7 @@ namespace MVVMSidekick.Behaviors
 
 				eventSubscribe = EventRouting.EventRouter.Instance
 					   .GetEventChannel<EventCommandEventArgs>()
-					   .ObserveOnDispatcher()
+					   .ObserveOn(System.Reactive.Concurrency.DispatcherScheduler.Current)
 					   .Where(_ =>
 						   IsBindingActive)
 					   .Where(x =>
