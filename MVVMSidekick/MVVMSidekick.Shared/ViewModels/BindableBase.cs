f0f215ce78ff3347aec1989440aff948161b141d
waynebaby <blackshaman_wayne@hotmail.com>
2016-12-20 13:39:12 +0800

    mini update about Command Model

---
+++ MVVMSidekick/MVVMSidekick.Shared/ViewModels/BindableBase.cs
@@ -0,0 +1,255 @@
+﻿using MVVMSidekick.EventRouting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace MVVMSidekick.ViewModels
+{
+
+    /// <summary>
+    /// <para>Base type of bindable model.</para>
+    /// <para>ViewModel 基类</para>
+    /// </summary>
+    [DataContract]
+    public abstract class BindableBase
+        : DisposeGroupBase, INotifyPropertyChanged, IBindable
+    {
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources.
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+        public BindableBase()
+        {
+            ValueContainers = new ValueContainerIndexer(this);
+        }
+        public ValueContainerIndexer ValueContainers { get; private set; }
+
+        /// <summary>
+        /// Occurs when [_ errors changed].
+        /// </summary>
+        protected event EventHandler<DataErrorsChangedEventArgs> _ErrorsChanged;
+        /// <summary>
+        /// Raises the errors changed.
+        /// </summary>
+        /// <param name="propertName">Name of the propert.</param>
+        protected internal void RaiseErrorsChanged(string propertName)
+        {
+            if (_ErrorsChanged != null)
+            {
+                _ErrorsChanged(this, new DataErrorsChangedEventArgs(propertName));
+            }
+        }
+
+        /// <summary>
+        /// Gets the bindable instance identifier.
+        /// </summary>
+        /// <value>The bindable instance identifier.</value>
+        abstract public String BindableInstanceId { get; }
+
+
+
+
+
+        /// <summary>
+        /// The _ is validation activated
+        /// </summary>
+        private bool _IsValidationActivated = false;
+        /// <summary>
+        /// <para>Gets ot sets if the validation is activatied. This is a flag only， internal logic is not depend on this.</para>
+        /// <para>读取/设置 此模型是否激活验证。这只是一个标记，内部逻辑并没有参考这个值</para>
+        /// </summary>
+        /// <value><c>true</c> if this instance is validation activated; otherwise, <c>false</c>.</value>
+        public bool IsValidationActivated
+        {
+            get { return _IsValidationActivated; }
+            set { _IsValidationActivated = value; }
+        }
+
+        /// <summary>
+        /// The _ is notification activated
+        /// </summary>
+        private bool _IsNotificationActivated = true;
+        /// <summary>
+        /// <para>Gets ot sets if the property change notification is activatied. </para>
+        /// <para>读取/设置 此模型是否激活变化通知</para>
+        /// </summary>
+        /// <value><c>true</c> if this instance is notification activated; otherwise, <c>false</c>.</value>
+        public bool IsNotificationActivated
+        {
+            get { return (!IsInDesignMode) ? _IsNotificationActivated : false; }
+            set { _IsNotificationActivated = value; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance is in design mode.
+        /// </summary>
+        /// <value><c>true</c> if this instance is in design mode; otherwise, <c>false</c>.</value>
+        public static bool IsInDesignMode { get { return Utilities.Runtime.IsInDesignMode; } }
+
+
+
+
+
+
+        ///// <summary>
+        /////  <para>0 for not disposed, 1 for disposed</para>
+        /////  <para>0 表示没有被Dispose 1 反之</para>
+        ///// </summary>
+        //private int disposedFlag = 0;
+
+        #region  Index and property names/索引与字段名
+        /// <summary>
+        /// <para>Get all property names that were defined in subtype, or added objectly in runtime</para>
+        /// <para>取得本VM实例已经定义的所有字段名。其中包括静态声明的和动态添加的。</para>
+        /// </summary>
+        /// <returns>String[]  Property names/字段名数组</returns>
+        public abstract string[] GetFieldNames();
+
+        ///// <summary>
+        ///// <para>Gets or sets  poperty values by property name index.</para>
+        ///// <para>使用索引方式取得/设置字段值</para>
+        ///// </summary>
+        ///// <param name="name">Property name/字段名</param>
+        ///// <returns>Property value/字段值</returns>
+        /// <summary>
+        /// Gets or sets the <see cref="System.Object"/> with the specified name.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns>System.Object.</returns>
+        public abstract object this[string name] { get; set; }
+
+
+        #endregion
+
+
+
+        #region Propery Changed Logic/ Propery Changed事件相关逻辑
+
+
+        /// <summary>
+        /// Raises the property changed.
+        /// </summary>
+        /// <param name="lazyEAFactory">The lazy ea factory.</param>
+        protected internal void RaisePropertyChanged(Func<PropertyChangedEventArgs> lazyEAFactory)
+        {
+
+
+            if (this.PropertyChanged != null)
+            {
+                var ea = lazyEAFactory();
+                this.PropertyChanged(this, ea);
+            }
+
+
+        }
+
+        /// <summary>
+        /// <para>Event that raised when properties were changed and Notification was activited</para>
+        /// <para> VM属性任何绑定用值被修改后,在启用通知情况下触发此事件</para>
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        #endregion
+
+        #region 验证与错误相关逻辑
+
+
+
+
+
+
+        ///// <summary>
+        ///// Checks the error.
+        ///// </summary>
+        ///// <param name="test">The test.</param>
+        ///// <param name="errorMessage">The error message.</param>
+        ///// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        //protected bool CheckError(Func<Boolean> test, string errorMessage)
+        //{
+
+        //    var rval = test();
+        //    if (rval)
+        //    {
+        //        SetErrorAndTryNotify(errorMessage);
+        //    }
+        //    return rval;
+
+        //}
+
+
+        ///// <summary>
+        ///// 验证错误内容
+        ///// </summary>
+        //string IDataErrorInfo.ErrorMessage
+        //{
+        //    get
+        //    {
+        //        return GetError();
+        //    }
+
+
+        //}
+        /// <summary>
+        /// <para>Gets the validate error of this model </para>
+        /// <para>取得错误内容</para>
+        /// </summary>
+        /// <value>The error.</value>
+        public abstract string ErrorMessage { get; }
+        /// <summary>
+        /// <para>Sets the validate error of this model </para>
+        /// <para>设置错误内容</para>
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>ErrorMessage string/错误内容字符串</returns>
+        protected abstract void SetErrorMessage(string value);
+
+        /// <summary>
+        /// <para>Sets the validate error of this model and notify </para>
+        /// <para>设置错误内容并且尝试用事件通知</para>
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>ErrorMessage string/错误内容字符串</returns>
+        protected abstract void SetErrorMessageAndTryNotify(string value);
+
+
+
+        public abstract IValueContainer GetValueContainer(string propertyName);
+
+
+
+
+        #endregion
+
+
+        //   public abstract bool IsUIBusy { get; set; }
+
+
+
+
+
+
+        /// <summary>
+        /// The Event Router that effects only in this Model Object/本Model生效的EventRouter
+        /// </summary>
+        public abstract EventRouter LocalEventRouter { get; set; }
+
+
+        /// <summary>
+        /// The Event Router that effects Globally /全局生效的Event Router引用
+        /// </summary>
+        /// <value>The global event router.</value>
+
+        public EventRouter GlobalEventRouter
+        {
+            get { return EventRouter.Instance; }
+        }
+
+    }
+}
---
+++ MVVMSidekick/MVVMSidekick.Shared/ViewModels/BindableBaseExtensions.cs
@@ -0,0 +1,139 @@
+﻿using MVVMSidekick.Common;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MVVMSidekick.ViewModels
+{
+
+    /// <summary>
+    /// <para>Extension methods of models</para>
+    /// <para>为Model增加的一些快捷方法</para>
+    /// </summary>
+    public static class BindableBaseExtensions
+    {
+
+
+
+        /// <summary>
+        /// <para>Config Value Container with delegate</para>
+        /// <para>使用连续的API设置ValueContainer的一些参数</para>
+        /// </summary>
+        /// <typeparam name="TProperty">ValueContainer内容的类型</typeparam>
+        /// <param name="target">ValueContainer的配置目标实例</param>
+        /// <param name="action">配置内容</param>
+        /// <returns>ValueContainer的配置目标实例</returns>
+        public static ValueContainer<TProperty> Config<TProperty>(this ValueContainer<TProperty> target, Action<ValueContainer<TProperty>> action)
+        {
+            action(target);
+            return target;
+        }
+
+        /// <summary>
+        /// <para>Add Idisposeable to model's despose action list</para>
+        /// <para>将IDisposable 对象注册到VM中的销毁对象列表。</para>
+        /// </summary>
+        /// <typeparam name="T">Type of Model /Model的类型</typeparam>
+        /// <param name="item">IDisposable Inastance/IDisposable实例</param>
+        /// <param name="targetGroup">The tg.</param>
+        /// <param name="needCheckInFinalizer">if set to <c>true</c> [need check in finalizer].</param>
+        /// <param name="comment">The comment.</param>
+        /// <param name="caller">The caller.</param>
+        /// <param name="file">The file.</param>
+        /// <param name="line">The line.</param>
+        /// <returns>T.</returns>
+        public static T DisposeWith<T>(this T item, IDisposeGroup targetGroup, bool needCheckInFinalizer = false, string comment = "", [CallerMemberName] string caller = "", [CallerFilePath] string file = "", [CallerLineNumber] int line = -1) where T : IDisposable
+        {
+
+            targetGroup.AddDisposable(item, needCheckInFinalizer, comment, caller, file, line);
+            return item;
+
+
+        }
+
+
+        /// <summary>
+        /// <para>注册在目标VM绑定的视图Unload的时候Dispose </para>
+        /// <para>Register a dispose object that would dispose when target viewmodel's view unload</para>
+        /// </summary>
+        /// <typeparam name="T"><para>任意IDisposable对象类型</para><para>some IDisposable Type</para></typeparam>
+        /// <param name="item"><para>注册的Disposeable对象</para><para>Disposable instance that would be registered</para></param>
+        /// <param name="viewModel">注册到的View Model</param>
+        /// <param name="needCheckInFinalizer">if set to <c>true</c> [need check in finalizer].</param>
+        /// <param name="comment">The comment.</param>
+        /// <param name="caller">The caller.</param>
+        /// <param name="file">The file.</param>
+        /// <param name="line">The line.</param>
+        /// <returns>T.</returns>
+        public static T DisposeWhenUnload<T>(this T item, IViewModelLifetime viewModel, bool needCheckInFinalizer = false, string comment = "", [CallerMemberName] string caller = "", [CallerFilePath] string file = "", [CallerLineNumber] int line = -1) where T : IDisposable
+        {
+
+            viewModel.UnloadDisposeGroup.AddDisposable(item, needCheckInFinalizer, comment, caller, file, line);
+            return item;
+
+
+        }
+        /// <summary>
+        /// <para>注册在目标VM绑定的视图与VM解除绑定的的时候Dispose </para>
+        /// <para>Register a dispose object that would dispose when target viewmodel's view unbind with the viewmodel</para>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="item"></param>
+        /// <param name="needCheckInFinalizer">if set to <c>true</c> [need check in finalizer].</param>
+        /// <param name="comment">The comment.</param>
+        /// <param name="caller">The caller.</param>
+        /// <param name="file">The file.</param>
+        /// <param name="line">The line.</param>
+        /// <returns>T.</returns>
+        public static T DisposeWhenUnbind<T>(this T item, IViewModelLifetime viewModel, bool needCheckInFinalizer = false, string comment = "", [CallerMemberName] string caller = "", [CallerFilePath] string file = "", [CallerLineNumber] int line = -1) where T : IDisposable
+        {
+
+            viewModel.UnbindDisposeGroup.AddDisposable(item, needCheckInFinalizer, comment, caller, file, line);
+            return item;
+
+
+        }
+
+        /// <summary>
+        /// Initializes the specified property name.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="model">The model.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <param name="reference">The reference.</param>
+        /// <param name="locator">The locator.</param>
+        /// <param name="defaultValueFactory">The default value factory.</param>
+        /// <returns>ValueContainer&lt;T&gt;.</returns>
+        public static ValueContainer<T> Initialize<T>(this BindableBase model, string propertyName, ref Property<T> reference, ref Func<BindableBase, ValueContainer<T>> locator, Func<T> defaultValueFactory = null)
+        {
+            if (reference == null)
+                reference = new Property<T> { LocatorFunc = locator };
+            if (reference.Container == null)
+            {
+                reference.Container = new ValueContainer<T>(propertyName, model);
+                if (defaultValueFactory != null)
+                {
+                    reference.Container.Value = defaultValueFactory();
+                }
+            }
+            return reference.Container;
+        }
+
+        /// <summary>
+        /// Initializes the specified property name.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="model">The model.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <param name="reference">The reference.</param>
+        /// <param name="locator">The locator.</param>
+        /// <param name="defaultValueFactory">The default value factory.</param>
+        /// <returns>ValueContainer&lt;T&gt;.</returns>
+        public static ValueContainer<T> Initialize<T>(this BindableBase model, string propertyName, ref Property<T> reference, ref Func<BindableBase, ValueContainer<T>> locator, Func<BindableBase, T> defaultValueFactory = null)
+        {
+            return Initialize(model, propertyName, ref reference, ref locator, () => (defaultValueFactory != null) ? defaultValueFactory(model) : default(T));
+        }
+    }
+
+}
