6f69988c6fa7952e0c2058e4a523c02ac3b3a0aa
waynebaby <blackshaman_wayne@hotmail.com>
2017-06-13 17:38:38 +0800

    test project :playground

---
+++ MVVMSidekick/MVVMSidekick.Test.Playground/BlankPage1.xaml.cs
@@ -0,0 +1,72 @@
+﻿
+using MVVMSidekick.Test.Playground.ViewModels;
+using System.Reactive;
+using System.Reactive.Linq;
+using MVVMSidekick.ViewModels;
+using MVVMSidekick.Views;
+using MVVMSidekick.Reactive;
+using MVVMSidekick.Services;
+using MVVMSidekick.Commands;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MVVMSidekick.Test.Playground
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BlankPage1 : MVVMPage
+    {
+
+
+
+        public BlankPage1()
+        {
+
+            this.InitializeComponent();
+            this.RegisterPropertyChangedCallback(ViewModelProperty, (_, __) =>
+            {
+                StrongTypeViewModel = this.ViewModel as BlankPage1_Model;
+            });
+            StrongTypeViewModel = this.ViewModel as BlankPage1_Model;
+        }
+
+
+        public BlankPage1_Model StrongTypeViewModel
+        {
+            get { return (BlankPage1_Model)GetValue(StrongTypeViewModelProperty); }
+            set { SetValue(StrongTypeViewModelProperty, value); }
+        }
+
+        public static readonly DependencyProperty StrongTypeViewModelProperty =
+                    DependencyProperty.Register("StrongTypeViewModel", typeof(BlankPage1_Model), typeof(BlankPage1), new PropertyMetadata(null));
+
+
+
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+        }
+
+    }
+}
---
+++ MVVMSidekick/MVVMSidekick.Test.Playground/Startups/BlankPage2.cs
@@ -0,0 +1,33 @@
+﻿using System.Reactive;
+using System.Reactive.Linq;
+using MVVMSidekick.ViewModels;
+using MVVMSidekick.Views;
+using MVVMSidekick.Reactive;
+using MVVMSidekick.Services;
+using MVVMSidekick.Commands;
+using MVVMSidekick.Test.Playground;
+using MVVMSidekick.Test.Playground.ViewModels;
+using System;
+using System.Net;
+using System.Windows;
+
+
+namespace MVVMSidekick.Startups
+{
+    internal static partial class StartupFunctions
+    {
+        static Action BlankPage2Config =
+           CreateAndAddToAllConfig(ConfigBlankPage2);
+
+        public static void ConfigBlankPage2()
+        {
+            ViewModelLocator<BlankPage2_Model>
+                .Instance
+                .Register(context =>
+                    new BlankPage2_Model())
+                .GetViewMapper()
+                .MapToDefault<BlankPage2>();
+
+        }
+    }
+}
