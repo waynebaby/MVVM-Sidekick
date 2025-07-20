191c69b655e02e5abfb1d749060a85c29915bd9d
wayne <waynebaby.wang@outlook.com>
2025-07-20 15:41:45 +0800

    Fix: Resolve build issues for the solution
    This commit resolves several build failures that occurred when building the MVVMSidekick.sln solution.
    
    - Added RuntimeIdentifier and RuntimeIdentifiers to the VSExtensions2019/MVVM-Sidekick-Extensions/MVVM-Sidekick-Extensions.csproj file to fix NuGet restore errors.
    
    - Added a PackageReference to Microsoft.WindowsAppSDK in the MVVMSidekick_WinUI3/MVVMSidekick_WinUI3.csproj file.
    
    The solution now compiles successfully using 'msbuild' from a Developer Command Prompt for VS 2022.

---
+++ MVVMSidekick_MAUI/Behaviors/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMSidekick_MAUI.Behaviors
+{
+    public class ButtonClickedEventMessageBehavior : Behavior<Button>
+    {
+        public ButtonClickedEventMessageBehavior()
+        {
+
+        }
+
+        public static readonly BindableProperty EventMessageProperty =
+            BindableProperty.Create(
+                nameof(EventMessage),
+                typeof(string),
+                typeof(ButtonClickedEventMessageBehavior));
+
+        public string EventMessage
+        {
+            get => (string)GetValue(EventMessageProperty);
+            set => SetValue(EventMessageProperty, value);
+        }
+
+ 
+        public static readonly BindableProperty EventRouterProperty =
+        BindableProperty.Create(
+            nameof(EventRouter),
+            typeof(MVVMSidekick.EventRouting.EventRouter),
+            typeof(ButtonClickedEventMessageBehavior), MVVMSidekick.EventRouting.EventRouter.Instance);
+        public MVVMSidekick.EventRouting.EventRouter EventRouter
+        {
+            get => (MVVMSidekick.EventRouting.EventRouter)GetValue(EventRouterProperty);
+            set => SetValue(EventRouterProperty, value);
+        }
+
+        public Button? AttachedObject
+        {
+            get; private set;
+        }
+        protected override void OnAttachedTo(Button bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.Clicked += Bindable_Clicked;
+            AttachedObject = bindable;
+        }
+        protected override void OnDetachingFrom(Button bindable)
+        {
+
+            bindable.Clicked -= Bindable_Clicked;
+            AttachedObject = null;
+            base.OnDetachingFrom(bindable);
+        }
+
+        private void Bindable_Clicked(object? sender, EventArgs e)
+        {
+            (EventRouter?? MVVMSidekick.EventRouting.EventRouter.Instance).RaiseEvent(sender, e, typeof(EventArgs), EventMessage, true, false);
+        }
+    }
+}
---
+++ MVVMSidekick_MAUI/ReactivePlatformExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMSidekick_MAUI
+{
+    public static class ReactivePlatformExtensions
+    {
+
+        public static IDisposable SubscribeOnDispatcher<T>(this IObservable<T> source, IDispatcher dispatcher, Action<T> onNext)
+        {
+            return source.Subscribe(async e => await dispatcher.DispatchAsync(() => onNext(e)));
+        }
+
+
+        public static void DisposeWhenUnload(this IDisposable disposable, VisualElement element)
+        {
+            element.Unloaded += async (s, e) =>
+            {
+                await element.Dispatcher.DispatchAsync(() => disposable.Dispose());
+            };
+
+
+        }
+    }
+}
